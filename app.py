2e7474e37615b29e925ea5a588d877fe7753000a
Yeshwant_Kumar <56618271+gullayeshwantkumarruler@users.noreply.github.com>
2021-11-03 20:53:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -30,38 +30,37 @@ with st.spinner('Model is being loaded..'):
 
 # Function for prediction
 def import_and_predict(image_data, model):
-
-        size = (128,128)
-        image = ImageOps.fit(image_data, size, Image.ANTIALIAS)
-        image = np.asarray(image)
-        img = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-        img_reshape = img[np.newaxis,...]
-        prediction = model.predict(img_reshape)
-        return prediction
+    size = (128,128)
+    image = ImageOps.fit(image_data, size, Image.ANTIALIAS)
+    image = np.asarray(image)
+    img = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    img_reshape = img[np.newaxis,...]
+    prediction = model.predict(img_reshape)
+    return prediction
 def main():
-      st.title("Monument Image Classifier")
-      html_temp = """
-      <div style="background-color:tomato;padding:10px">
-      <h2 style="color:white;text-align:center;">Monument Image Classifier App </h2>
-      </div>
-      """
-      st.markdown(html_temp,unsafe_allow_html=True)
-      file = st.file_uploader("Please upload an image", type=["jpg", "png"])
-      class_names=['Angkor_wat','Buckingham_Palace','Burj_khalifa','Christ_the_Redeemer','Gateway_of_India','Niagara_Falls','Tajmahal','The_Eiffel_Tower','The_Great_Wall_of_China','The_Sydney_Opera_House']
-      result=""
-      if st.button("Predict"):
-  #         if file is None:
-  #           st.text("Please upload an image file")
-  #         else:
-            image = Image.open(file)
-            st.image(image, use_column_width=True)
-            predictions = import_and_predict(image,model)
-            score = tf.nn.softmax(predictions[0])
-            result=class_names[np.argmax(score)
-       st.success('The output is {}'.format(result))
-       if st.button("About"):
-          st.text("Lets LEarn")
-          st.text("Built with Streamlit")
+    st.title("Monument Image Classifier")
+    html_temp = """
+    <div style="background-color:tomato;padding:10px">
+    <h2 style="color:white;text-align:center;">Monument Image Classifier App </h2>
+    </div>
+    """
+    st.markdown(html_temp,unsafe_allow_html=True)
+    file = st.file_uploader("Please upload an image", type=["jpg", "png"])
+    class_names=['Angkor_wat','Buckingham_Palace','Burj_khalifa','Christ_the_Redeemer','Gateway_of_India','Niagara_Falls','Tajmahal','The_Eiffel_Tower','The_Great_Wall_of_China','The_Sydney_Opera_House']
+    result=""
+    if st.button("Predict"):
+#         if file is None:
+#           st.text("Please upload an image file")
+#         else:
+        image = Image.open(file)
+        st.image(image, use_column_width=True)
+        predictions = import_and_predict(image,model)
+        score = tf.nn.softmax(predictions[0])
+        result=class_names[np.argmax(score)
+    st.success('The output is {}'.format(result))
+    if st.button("About"):
+       st.text("Lets LEarn")
+       st.text("Built with Streamlit")
 
 if __name__=='__main__':
     main()
