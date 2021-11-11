89c1ee319fe252a69d597ec5b1c07a68b37b8747
Yeshwant_Kumar <56618271+gullayeshwantkumarruler@users.noreply.github.com>
2021-11-11 13:16:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -48,57 +48,97 @@ def main():
         result= class_names[np.argmax(score)]
         st.write('This is {}'.format(result))
         st.caption("The result is trained on similar images like: ")
-        if result=='Angkor_wat':
-          images=["Angkor_wat/Angkor_wat (1).jpg","Angkor_wat/Angkor_wat (2).jpg","Angkor_wat/Angkor_wat (3).jpg","Angkor_wat/Angkor_wat (4).jpg"]
-          for i in images:
-            image = Image.open(i).resize((100, 100))
+        
+        train_path_Angkor_wat=[]
+        train_path_Buckingham_Palace=[]
+        train_path_Burj_khalifa=[]
+        train_path_Christ_the_Redeemer=[]
+        train_path_Gateway_of_India=[]
+        train_path_Niagara_Falls=[]
+        train_path_Tajmahal=[]
+        train_path_The_Eiffel_Tower=[]
+        train_path_The_Great_Wall_of_China=[]
+        train_path_The_Sydney_Opera_House=[]
+        
+        for folder_name in ['/Angkor_wat/','/Buckingham_Palace/','/Burj_khalifa/','/Christ_the_Redeemer/','/Gateway_of_India/','/Niagara_Falls/','/Tajmahal/','/The_Eiffel_Tower/','/The_Great_Wall_of_China/','/The_Sydney_Opera_House/']:
+    
+          #Path of the folder
+          images_path = os.listdir(train_dir + folder_name)
+
+          for i, image_name in enumerate(images_path): 
+            if folder_name=='/Angkor_wat/':
+                train_path_Angkor_wat.append(0)
+            elif folder_name=='/Buckingham_Palace/':
+                train_path_Buckingham_Palace.append(1)
+            elif folder_name=='/Burj_khalifa/':
+                train_path_Burj_khalifa.append(2)
+            elif folder_name=='/Christ_the_Redeemer/':
+                train_path_Christ_the_Redeemer.append(3)
+            elif folder_name=='/Gateway_of_India/':
+                train_path_Gateway_of_India.append(4)
+            elif folder_name=='/Niagara_Falls/':
+                train_path_Niagara_Falls.append(5)
+            elif folder_name=='/Tajmahal/':
+                train_path_Tajmahal.append(6)
+            elif folder_name=='/The_Eiffel_Tower/':
+                train_path_The_Eiffel_Tower.append(7)
+            elif folder_name=='/The_Great_Wall_of_China/':
+                train_path_The_Great_Wall_of_China.append(8)
+            elif folder_name=='/The_Sydney_Opera_House/':
+                train_path_The_Sydney_Opera_House.append(9)
+
+        st.write(train_path_The_Sydney_Opera_House)
+#         if result=='Angkor_wat':
+#           images=["Angkor_wat/Angkor_wat (1).jpg","Angkor_wat/Angkor_wat (2).jpg","Angkor_wat/Angkor_wat (3).jpg","Angkor_wat/Angkor_wat (4).jpg"]
+#           for i in images:
+#             image = Image.open(i).resize((100, 100))
             
-            st.image(image,caption='Angkor_wat')
-        elif result=='Buckingham_Palace':
-          images=["Buckingham_Palace/Buckingham_Palace (1).jpg","Buckingham_Palace/Buckingham_Palace (2).jpg","Buckingham_Palace/Buckingham_Palace (3).jpg","Buckingham_Palace/Buckingham_Palace (4).jpg"]
-          for i in images:
-            image = Image.open(i).resize((100, 100))
-            st.image(image, caption='Buckingham_Palace')
-        elif result=='Burj_khalifa':
-          images=["Burj_khalifa/Burj_khalifa (1).jpg","Burj_khalifa/Burj_khalifa (2).jpg","Burj_khalifa/Burj_khalifa (3).jpg","Burj_khalifa/Burj_khalifa (4).jpg"]
-          for i in images:
-            image = Image.open(i).resize((100, 100))
-            st.image(image, caption='Burj_khalifa')
-        elif result=='Christ_the_Redeemer':
-          images=["Christ_the_Redeemer/Christ_the_Redeemer (1).jpg","Christ_the_Redeemer/Christ_the_Redeemer (2).jpg","Christ_the_Redeemer/Christ_the_Redeemer (3).jpg","Christ_the_Redeemer/Christ_the_Redeemer (4).jpg"]
-          for i in images:
-            image = Image.open(i).resize((100, 100))
-            st.image(image, caption='Christ_the_Redeemer')
-        elif result=='Gateway_of_India':
-          images=["Gateway_of_India/Gateway_of_India (1).jpg","Gateway_of_India/Gateway_of_India (2).jpg","Gateway_of_India/Gateway_of_India (3).jpg","Gateway_of_India/Gateway_of_India (4).jpg"]
-          for i in images:
-            image = Image.open(i).resize((100, 100))
-            st.image(image, caption='Gateway_of_India')
-        elif result=='Niagara_Falls':
-          images=["Niagara_Falls/Niagara_Falls (1).jpg","Niagara_Falls/Niagara_Falls (2).jpg","Niagara_Falls/Niagara_Falls (3).jpg","Niagara_Falls/Niagara_Falls (4).jpg"]
-          for i in images:
-            image = Image.open(i).resize((100, 100))
-            st.image(image, caption='Niagara_Falls')
-        elif result=='Tajmahal':
-          images=["Tajmahal/Tajmahal (1).jpg","Tajmahal/Tajmahal (2).jpg","Tajmahal/Tajmahal (3).jpg","Tajmahal/Tajmahal (4).jpg"]
-          for i in images:
-            image = Image.open(i).resize((100, 100))
-            st.image(image, caption='Tajmahal')
-        elif result=='The_Eiffel_Tower':
-          images=["The_Eiffel_Tower/The_Eiffel_Tower (1).jpg","The_Eiffel_Tower/The_Eiffel_Tower (2).jpg","The_Eiffel_Tower/The_Eiffel_Tower (3).jpg","The_Eiffel_Tower/The_Eiffel_Tower (4).jpg"]
-          for i in images:
-            image = Image.open(i).resize((100, 100))
-            st.image(image, caption='The_Eiffel_Tower')
-        elif result=='The_Great_Wall_of_China':
-          images=["The_Great_Wall_of_China/The_Great_Wall_of_China (1).jpg","The_Great_Wall_of_China/The_Great_Wall_of_China (2).jpg","The_Great_Wall_of_China/The_Great_Wall_of_China (3).jpg","The_Great_Wall_of_China/The_Great_Wall_of_China (4).jpg"]
-          for i in images:
-            image = Image.open(i).resize((100, 100))
-            st.image(image, caption='The_Great_Wall_of_China')
-        elif result=='The_Sydney_Opera_House':
-          images=["The_Sydney_Opera_House/The_Sydney_Opera_House (1).jpg","The_Sydney_Opera_House/The_Sydney_Opera_House (2).jpg","The_Sydney_Opera_House/The_Sydney_Opera_House (3).jpg","The_Sydney_Opera_House/The_Sydney_Opera_House (4).jpg"]
-          for i in images:
-            image = Image.open(i).resize((100, 100))
-            st.image(image, caption='The_Sydney_Opera_House')
+#             st.image(image,caption='Angkor_wat')
+#         elif result=='Buckingham_Palace':
+#           images=["Buckingham_Palace/Buckingham_Palace (1).jpg","Buckingham_Palace/Buckingham_Palace (2).jpg","Buckingham_Palace/Buckingham_Palace (3).jpg","Buckingham_Palace/Buckingham_Palace (4).jpg"]
+#           for i in images:
+#             image = Image.open(i).resize((100, 100))
+#             st.image(image, caption='Buckingham_Palace')
+#         elif result=='Burj_khalifa':
+#           images=["Burj_khalifa/Burj_khalifa (1).jpg","Burj_khalifa/Burj_khalifa (2).jpg","Burj_khalifa/Burj_khalifa (3).jpg","Burj_khalifa/Burj_khalifa (4).jpg"]
+#           for i in images:
+#             image = Image.open(i).resize((100, 100))
+#             st.image(image, caption='Burj_khalifa')
+#         elif result=='Christ_the_Redeemer':
+#           images=["Christ_the_Redeemer/Christ_the_Redeemer (1).jpg","Christ_the_Redeemer/Christ_the_Redeemer (2).jpg","Christ_the_Redeemer/Christ_the_Redeemer (3).jpg","Christ_the_Redeemer/Christ_the_Redeemer (4).jpg"]
+#           for i in images:
+#             image = Image.open(i).resize((100, 100))
+#             st.image(image, caption='Christ_the_Redeemer')
+#         elif result=='Gateway_of_India':
+#           images=["Gateway_of_India/Gateway_of_India (1).jpg","Gateway_of_India/Gateway_of_India (2).jpg","Gateway_of_India/Gateway_of_India (3).jpg","Gateway_of_India/Gateway_of_India (4).jpg"]
+#           for i in images:
+#             image = Image.open(i).resize((100, 100))
+#             st.image(image, caption='Gateway_of_India')
+#         elif result=='Niagara_Falls':
+#           images=["Niagara_Falls/Niagara_Falls (1).jpg","Niagara_Falls/Niagara_Falls (2).jpg","Niagara_Falls/Niagara_Falls (3).jpg","Niagara_Falls/Niagara_Falls (4).jpg"]
+#           for i in images:
+#             image = Image.open(i).resize((100, 100))
+#             st.image(image, caption='Niagara_Falls')
+#         elif result=='Tajmahal':
+#           images=["Tajmahal/Tajmahal (1).jpg","Tajmahal/Tajmahal (2).jpg","Tajmahal/Tajmahal (3).jpg","Tajmahal/Tajmahal (4).jpg"]
+#           for i in images:
+#             image = Image.open(i).resize((100, 100))
+#             st.image(image, caption='Tajmahal')
+#         elif result=='The_Eiffel_Tower':
+#           images=["The_Eiffel_Tower/The_Eiffel_Tower (1).jpg","The_Eiffel_Tower/The_Eiffel_Tower (2).jpg","The_Eiffel_Tower/The_Eiffel_Tower (3).jpg","The_Eiffel_Tower/The_Eiffel_Tower (4).jpg"]
+#           for i in images:
+#             image = Image.open(i).resize((100, 100))
+#             st.image(image, caption='The_Eiffel_Tower')
+#         elif result=='The_Great_Wall_of_China':
+#           images=["The_Great_Wall_of_China/The_Great_Wall_of_China (1).jpg","The_Great_Wall_of_China/The_Great_Wall_of_China (2).jpg","The_Great_Wall_of_China/The_Great_Wall_of_China (3).jpg","The_Great_Wall_of_China/The_Great_Wall_of_China (4).jpg"]
+#           for i in images:
+#             image = Image.open(i).resize((100, 100))
+#             st.image(image, caption='The_Great_Wall_of_China')
+#         elif result=='The_Sydney_Opera_House':
+#           images=["The_Sydney_Opera_House/The_Sydney_Opera_House (1).jpg","The_Sydney_Opera_House/The_Sydney_Opera_House (2).jpg","The_Sydney_Opera_House/The_Sydney_Opera_House (3).jpg","The_Sydney_Opera_House/The_Sydney_Opera_House (4).jpg"]
+#           for i in images:
+#             image = Image.open(i).resize((100, 100))
+#             st.image(image, caption='The_Sydney_Opera_House')
 
           
        
