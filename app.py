a9dd8e96dbe48dd2e40314bff4b2e0dc74dc6523
Yeshwant_Kumar <56618271+gullayeshwantkumarruler@users.noreply.github.com>
2021-11-11 14:04:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -94,6 +94,44 @@ def main():
           image = Image.open(i).resize((100, 100))
           The_Sydney_Opera_House.append(image)
         st.image(The_Sydney_Opera_House)
+        
+        
+        with st.sidebar:
+            st.header("Configuration")
+            with st.form(key="grid_reset"):
+                n_photos = st.slider("Number of cat photos:", 4, 128, 16)
+                n_cols = st.number_input("Number of columns", 2, 8, 4)
+                st.form_submit_button(label="Reset images and layout")
+#             with st.expander("About this app"):
+#                 st.markdown("It's about cats :cat:!")
+#             st.caption("Source: https://cataas.com/#/")
+
+#         st.title("Choose your favorite cat :cat:")
+#         st.caption(
+#             "You can display the image in full size by hovering it and clicking the double arrow"
+#         )
+
+        cat_images =The_Sydney_Opera_House
+        n_rows = 1 + len(cat_images) // int(n_cols)
+        rows = [st.container() for _ in range(n_rows)]
+        cols_per_row = [r.columns(n_cols) for r in rows]
+        cols = [column for row in cols_per_row for column in row]
+
+        for image_index, cat_image in enumerate(cat_images):
+            cols[image_index].image(cat_image)
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
 #         if result=='Angkor_wat':
 #           images=["Angkor_wat/Angkor_wat (1).jpg","Angkor_wat/Angkor_wat (2).jpg","Angkor_wat/Angkor_wat (3).jpg","Angkor_wat/Angkor_wat (4).jpg"]
 #           for i in images:
