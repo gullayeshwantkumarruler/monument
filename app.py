502fba9ea3b7f084daf98857d508fb57e76173a2
Yeshwant_Kumar <56618271+gullayeshwantkumarruler@users.noreply.github.com>
2021-11-12 15:40:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -41,9 +41,9 @@ def main():
     final_images=""
     with st.sidebar:
       st.header("Monument Classifier using VGG16")
-      st.text("Please upload an image from one of these categories")
-      st.text("1. Angkor wat")
-      st.text("2. Buckingham Palace")
+      st.subheader("Please upload an image from one of these categories")
+      st.caption("1. Angkor wat")
+      st.caption("2. Buckingham Palace")
       st.text("3. Burj khalifa")
       st.image("vgg16.jpg")
             
